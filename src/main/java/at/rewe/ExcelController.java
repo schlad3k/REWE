195aa50ad7ffbc322b1e11f8ffa9d6bf0a957b69
x2fresh <x2fresh@rewe-group.at>
2024-11-27 10:25:54 +0100

    update readme

--- src/main/java/at/rewe/ExcelController.java
+++ src/main/java/at/rewe/ExcelController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,10 +16,18 @@ public class ExcelController {
 
     @Autowired
     private ExcelReader excelReader;
-
+//curl -X POST http://localhost:8080/riag/supply/article-mdm/tgm-x2fresh/backend/api/excel/uploadFile \ -F "file=@/Users/TGM_x2FRESH/Documents/Stammdatenblätter/ARTIKEL-STAMMBLATT (1).xlsx"
     @PostMapping("/uploadFile")
-    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file){
-        return ResponseEntity.ok().body("file received successfully");
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+            List<String> list = new ArrayList<>();
+            list.add("I15");
+            list.add("BQ54");
+            // Call the ExcelReader to read the values
+            List<String> cellValues = excelReader.readCellsFromUpload(file, list);
+
+            return ResponseEntity.ok(cellValues.toString());
+
+
     }
     /*
     @PostMapping("/upload")
